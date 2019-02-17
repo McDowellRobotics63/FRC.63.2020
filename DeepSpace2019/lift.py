b39fdc87e119c158b1a140d59d745196fc855798
joewak <joe.wakeman@gmail.com>
2019-02-17 16:42:15 +0000

    Run the ilft

--- DeepSpace2019/lift.py
+++ DeepSpace2019/lift.py
@@ -1,6 +1,11 @@
 
 import wpilib
 
+from ctre import TalonSRX
+from ctre import ControlMode
+from ctre import FeedbackDevice
+from ctre._impl import StatusFrame
+
 class DeepSpaceLift():
 
   def __init__(self, logger):
@@ -8,14 +13,24 @@ class DeepSpaceLift():
 
   def init(self):
     self.logger.info("DeepSpaceLift::init()")
+    self.talon = TalonSRX(7)
     self.timer = wpilib.Timer()
     self.timer.start()
 
   def config(self):
     self.logger.info("DeepSpaceLift::config()")
+    self.talon.configNominalOutputForward(0.0, 10)
+    self.talon.configNominalOutputReverse(0.0, 10)
+    self.talon.configPeakOutputForward(1.0, 10)
+    self.talon.configPeakOutputReverse(-1.0, 10)
+    self.talon.enableVoltageCompensation(True)
+    self.talon.configVoltageCompSaturation(11.5, 10)
+    self.talon.configOpenLoopRamp(0.125, 10)
+    self.talon.setInverted(False)
 
   def iterate(self, pilot_stick, copilot_stick):
     self.logger.info("DeepSpaceLift::iterate()")
+    self.talon.set(ControlMode.PercentOutput, pilot_stick.getRawAxis(1))
 
   def disable(self):
     self.logger.info("DeepSpaceLift::disable()")
