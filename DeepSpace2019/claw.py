baffeb49ab1c687305b7c8af40082b87ea54d29c
joewak <joe.wakeman@gmail.com>
2019-02-20 03:03:23 +0000

    Switch wrist to pneumatics

--- DeepSpace2019/claw.py
+++ DeepSpace2019/claw.py
@@ -9,15 +9,16 @@ class DeepSpaceClaw():
   CLAW_STOW_RELEASE_BALL = 1
   CLAW_STOW_WAIT1 = 2
   CLAW_STOW_MOVE_TO_STOW = 3
-  CLAW_STOW_HOLD = 4
-  CLAW_DEPLOY_MOVE_TO_ACTIVE = 5
-  CLAW_DEPLOY_ACTIVE = 6
-  CLAW_SHOOT_BALL = 7
-  CLAW_SHOOT_BALL_WAIT1 = 8
-
+  CLAW_STOW_WAIT2 = 4
+  CLAW_STOW_HOLD = 5
+  CLAW_DEPLOY = 6
+  CLAW_DEPLOY_WAIT1 = 7
+  CLAW_DEPLOY_ACTIVE = 8
+  CLAW_SHOOT_BALL = 9
+  CLAW_SHOOT_BALL_WAIT1 = 10
 
   CLAW_STOW_BEGIN = CLAW_STOW_RELEASE_BALL
-  CLAW_DEPLOY_BEGIN = CLAW_DEPLOY_MOVE_TO_ACTIVE
+  CLAW_DEPLOY_BEGIN = CLAW_DEPLOY
 
   def __init__(self, logger):
     self.logger = logger
@@ -34,19 +35,20 @@ class DeepSpaceClaw():
     
     self.left_grab = ctre.TalonSRX(robotmap.CLAW_LEFT_WHEELS_CAN_ID)
     self.right_grab = ctre.TalonSRX(robotmap.CLAW_RIGHT_WHEELS_CAN_ID)
-    self.wrist_talon = ctre.TalonSRX(robotmap.CLAW_WRIST_CAN_ID)
 
-    self.talons = [self.left_grab, self.right_grab, self.wrist_talon]
-
-    '''Select and zero sensor in init() function.  That way the zero position doesn't get reset every time we enable/disable robot'''
-    self.wrist_talon.configSelectedFeedbackSensor(ctre.FeedbackDevice.Analog, 0, robotmap.CAN_TIMEOUT_MS)
-    self.wrist_talon.setSelectedSensorPosition(0, 0, robotmap.CAN_TIMEOUT_MS)
+    self.talons = [self.left_grab, self.right_grab]
 
     self.claw_open = wpilib.Solenoid(robotmap.PCM2_CANID, robotmap.CLAW_OPEN_SOLENOID)
     self.claw_close = wpilib.Solenoid(robotmap.PCM2_CANID, robotmap.CLAW_CLOSE_SOLENOID)
 
+    self.wrist_down = wpilib.Solenoid(robotmap.PCM2_CANID, robotmap.WRIST_EXTEND_SOLENOID)
+    self.wrist_up = wpilib.Solenoid(robotmap.PCM2_CANID, robotmap.WRIST_RETRACT_SOLENOID)
+
     self.claw_close.set(True)
     self.claw_open.set(False)
+
+    self.wrist_down.set(False)
+    self.wrist_up.set(True)
 
   def config(self, simulation):
     self.logger.info("DeepSpaceClaw::config(): ")
@@ -61,11 +63,6 @@ class DeepSpaceClaw():
       talon.configVoltageCompSaturation(11.5, robotmap.CAN_TIMEOUT_MS)
       talon.configOpenLoopRamp(0.125, robotmap.CAN_TIMEOUT_MS)
 
-    '''sensor config'''
-    self.wrist_talon.configSelectedFeedbackCoefficient(1.0, 0, robotmap.CAN_TIMEOUT_MS)
-    self.wrist_talon.setSensorPhase(True)
-    self.wrist_talon.setStatusFramePeriod(ctre._impl.StatusFrame.Status_2_Feedback0, 10, robotmap.CAN_TIMEOUT_MS)
-
   def deploy_claw(self):
     #Should we confirm currently in compatible state or allow interruption of sequence?
     self.current_state = self.CLAW_DEPLOY_BEGIN
@@ -79,14 +76,10 @@ class DeepSpaceClaw():
     self.current_state = self.CLAW_SHOOT_BALL
 
   def iterate(self, manual_mode, pilot_stick, copilot_stick):
-    wrist_position = self.wrist_talon.getSelectedSensorPosition(0)
 
     if self.timer.hasPeriodPassed(0.5):
       self.logger.info("DeepSpaceClaw::iterate()")
       self.logger.info("current state: " + str(self.current_state))
-      self.logger.info("wrist position: " + str(wrist_position))
-
-    
 
     #****************STOW SEQUENCE****************************
     if self.current_state == self.CLAW_STOW_RELEASE_BALL:
@@ -105,24 +98,32 @@ class DeepSpaceClaw():
     elif self.current_state == self.CLAW_STOW_MOVE_TO_STOW:
       self.left_grab.set(ctre.ControlMode.PercentOutput, 0.0)
       self.right_grab.set(ctre.ControlMode.PercentOutput, 0.0)
-      self.wrist_talon.set(ctre.ControlMode.PercentOutput, 0.0)
-      if wrist_position < 5:
+      self.wrist_down.set(False)
+      self.wrist_up.set(True)
+      self.state_timer.reset()
+      self.state_timer.start()
+      self.current_state = self.CLAW_STOW_WAIT2
+
+    elif self.current_state == self.CLAW_STOW_WAIT2:
+      if self.state_timer.hasPeriodPassed(0.25):
         self.current_state = self.CLAW_STOW_HOLD
     
     elif self.current_state == self.CLAW_STOW_HOLD:
       self.left_grab.set(ctre.ControlMode.PercentOutput, 0.0)
       self.right_grab.set(ctre.ControlMode.PercentOutput, 0.0)
-      self.wrist_talon.set(ctre.ControlMode.PercentOutput, 0.0)
     #****************STOW SEQUENCE****************************
     
     #****************DEPLOY SEQUENCE****************************
-    elif self.current_state == self.CLAW_DEPLOY_MOVE_TO_ACTIVE:
-      self.wrist_talon.set(ctre.ControlMode.PercentOutput, -0.1)
-      if wrist_position > 25:
+    elif self.current_state == self.CLAW_DEPLOY:
+      self.wrist_down.set(True)
+      self.wrist_up.set(False)
+      self.current_state = self.CLAW_DEPLOY_WAIT1
+
+    elif self.current_state == self.CLAW_DEPLOY_WAIT1:
+      if self.state_timer.hasPeriodPassed(0.25):
         self.current_state = self.CLAW_DEPLOY_ACTIVE
     
     elif self.current_state == self.CLAW_DEPLOY_ACTIVE:
-      self.wrist_talon.set(ctre.ControlMode.PercentOutput, 0.0)
       if not self.ball_infrared.get():
         self.left_grab.set(ctre.ControlMode.PercentOutput, 0.0)
         self.right_grab.set(ctre.ControlMode.PercentOutput, 0.0)
@@ -151,12 +152,8 @@ class DeepSpaceClaw():
       self.claw_close.set(False)
       self.claw_open.set(True)
     
-    if manual_mode == True:
-      self.wrist_talon.set(ctre.ControlMode.PercentOutput, copilot_stick.getRawAxis(robotmap.XBOX_RIGHT_Y_AXIS))
-
   def disable(self):
     self.logger.info("DeepSpaceClaw::disable()")
-    self.wrist_talon.set(ctre.ControlMode.PercentOutput, 0.0)
     self.left_grab.set(ctre.ControlMode.PercentOutput, 0.0)
     self.right_grab.set(ctre.ControlMode.PercentOutput, 0.0)
 
