01b1c3210f498dbc009534b457bf257094d78d42
joe <joe.wakeman@gmail.com>
2019-03-03 23:20:31 +0000

    Commit tested version of xboxcontroller class in DeepSpace2019

--- DeepSpace2019/robot.py
+++ DeepSpace2019/robot.py
@@ -37,43 +37,6 @@ class MyRobot(wpilib.TimedRobot):
     self.harpoon = DeepSpaceHarpoon(self.logger)
     self.harpoon.init()
 
-    '''
-    These are the values used in last year's code
-    Accessed by xboxcontroller.py for use in smoothing
-    '''
-    SmartDashboard.putString("DRIVE_VALUE", "DRIVE_VALUES")
-
-    SmartDashboard.putString("LEFT_Y_VALUES", "LEFT_Y_VALUES")
-    SmartDashboard.putNumber("left_y_rate", 0.6)
-    SmartDashboard.putNumber("left_y_expo", 1.0)
-    SmartDashboard.putNumber("left_y_deadband", 0.1)
-    SmartDashboard.putNumber("left_y_power", 1.5)
-    SmartDashboard.putNumber("left_y_min", -0.5)
-    SmartDashboard.putNumber("left_y_max", 0.5)
-
-    SmartDashboard.putString("LEFT_X_VALUES", "LEFT_X_VALUES")
-    SmartDashboard.putNumber("left_x_rate", 0.5)
-    SmartDashboard.putNumber("left_x_expo", 1.0)
-    SmartDashboard.putNumber("left_x_deadband", 0.1)
-    SmartDashboard.putNumber("left_x_power", 1.5)
-    SmartDashboard.putNumber("left_x_min", -0.5)
-    SmartDashboard.putNumber("left_x_max", 0.5)
-
-    SmartDashboard.putString("RIGHT_Y_VALUES", "RIGHT_Y_VALUES")
-    SmartDashboard.putNumber("right_y_rate", 1.0)
-    SmartDashboard.putNumber("right_y_expo", 0.0)
-    SmartDashboard.putNumber("right_y_deadband", 0.1)
-    SmartDashboard.putNumber("right_y_power", 1.0)
-    SmartDashboard.putNumber("right_y_min", -1.0)
-    SmartDashboard.putNumber("right_y_max", 1.0)
-		
-    SmartDashboard.putString("TURN", "TURN_VALUES")
-    SmartDashboard.putNumber("right_x_rate", 0.5)
-    SmartDashboard.putNumber("right_x_expo", 1.0)
-    SmartDashboard.putNumber("right_x_deadband", 0.1)
-    SmartDashboard.putNumber("right_x_power", 1.5)
-    SmartDashboard.putNumber("right_x_min", -0.5)
-    SmartDashboard.putNumber("right_x_max", 0.5)
 
   def autonomousInit(self):
     self.logger.info("MODE: autonomousInit")
